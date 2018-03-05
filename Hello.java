e43b1d94f9c2195b36671e5e86bbf253931a31b2
rekhapriyanka <rekhapriyankac@biarca.com>
2018-03-05 12:44:43 +0530

    Test2 added semicolon again

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, Teamcity")
+        System.out.println("Hello, Teamcity");
     }
 
 }
