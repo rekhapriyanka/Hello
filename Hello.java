8349d21adcd25ba746fe378e6cea5a530d61ebe0
rekhapriyanka <rekhapriyankac@biarca.com>
2018-03-08 12:12:37 +0530

    new print

--- Hello.java
+++ Hello.java
@@ -6,6 +6,7 @@ public class Hello {
         System.out.println("Automatically triggered");
         System.out.println("To the github teamcity is added2");
         System.out.println("To the github connection changing teamcity failing no response");
+        System.out.print("Integration and service");
                
     }
 
