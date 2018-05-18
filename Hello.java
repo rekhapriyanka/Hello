008aaf4bb08a8cbda18cb4d19a5cae8fd4cc7970
rekhapriyanka <rekhapriyankac@biarca.com>
2018-05-18 12:01:40 +0530

    for conflict

--- Hello.java
+++ Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         System.out.println("Automatically triggered");
         System.out.println("To the github teamcity is added2");
         System.out.println("To the github connection changing teamcity failing no response");
-        System.out.println("Integration and service");
+        System.out.println("Integration and service kadfisetuosdjgisdugyisd2w2w2s3e4e343dfrfr5tf5gtgt6");
         System.out.println("Testing fo tag creation initiated");
         System.out.println("testing");
         System.out.println("new integration and service new url added");
